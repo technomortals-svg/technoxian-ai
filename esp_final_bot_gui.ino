70015c156410668d7222f51861b1f431f214f6af
technomortals-svg <technomortals@gmail.com>
2026-04-12 11:56:22 +0530

    Add files via upload

---
+++ esp_final_bot_gui.ino
@@ -0,0 +1,44 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "YOUR_WIFI";
+const char* password = "YOUR_PASS";
+
+ESP8266WebServer server(80);
+
+#define IN1 D1
+#define IN2 D2
+#define IN3 D3
+#define IN4 D4
+
+void forward(){
+ digitalWrite(IN1,HIGH); digitalWrite(IN2,LOW);
+ digitalWrite(IN3,HIGH); digitalWrite(IN4,LOW);
+}
+
+void stopBot(){
+ digitalWrite(IN1,LOW); digitalWrite(IN2,LOW);
+ digitalWrite(IN3,LOW); digitalWrite(IN4,LOW);
+}
+
+void handle(){
+ String cmd = server.arg("cmd");
+ if(cmd=="FWD") forward();
+ else stopBot();
+ server.send(200,"text/plain","OK");
+}
+
+void setup(){
+ pinMode(IN1,OUTPUT); pinMode(IN2,OUTPUT);
+ pinMode(IN3,OUTPUT); pinMode(IN4,OUTPUT);
+
+ WiFi.begin(ssid,password);
+ while(WiFi.status()!=WL_CONNECTED) delay(500);
+
+ server.on("/control",handle);
+ server.begin();
+}
+
+void loop(){
+ server.handleClient();
+}
